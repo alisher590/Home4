7c7a456c05a81e24a839d9e11a73aa9b311e083a
алишер <aliser@MacBook-Air-aliser.local>
2023-03-19 05:06:32 -0700

    Add new home2

---
+++ home2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+try{
+int num = ReadNum("Введите число: ");
+int comand = NumberCount(num);
+SumNumbers(num, comand);
+int ReadNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int NumberCount(int a)
+{
+    int crash = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        crash++;
+    }
+    return crash;
+}
+void SumNumbers(int n, int comand)
+{
+    int result = 0;
+    for (int i = 1; i <= comand; i++)
+    {
+        result += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"{num} ->{result}");
+}
+}
+catch(Exception exc){
+    Console.Write($"Error Message!! -> {exc.Message}");
+}
